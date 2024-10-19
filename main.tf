4d90312f992fb274416e2238a8f89662efa25b12
agp19d <agp19d@gmail.com>
2024-10-19 16:38:16 -0500

    deploy vpc with terraform module

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# EC2 instance to provision in us-east-1
-resource "aws_instance" "ec2-instance1" {
-  ami           = "ami-06b21ccaeff8cd686" # Amazon linux 3
-  instance_type = "t2.micro"
+# # EC2 instance to provision in us-east-1
+# resource "aws_instance" "ec2-instance1" {
+#   ami           = "ami-06b21ccaeff8cd686" # Amazon linux 3
+#   instance_type = "t2.micro"
 
-  tags = {
-    name = "demo-server1"
-  }
-}
+#   tags = {
+#     name = "demo-server1"
+#   }
+# }
 
-# EC2 instance to provision in us-west-1
-resource "aws_instance" "ec2-instance2" {
-  provider      = aws.west
-  ami           = "ami-0cf4e1fcfd8494d5b" # Amazon linux 3
-  instance_type = "t2.micro"
+# # EC2 instance to provision in us-west-1
+# resource "aws_instance" "ec2-instance2" {
+#   provider      = aws.west
+#   ami           = "ami-0cf4e1fcfd8494d5b" # Amazon linux 3
+#   instance_type = "t2.micro"
 
-  tags = {
-    name = "demo-server2"
-  }
-}
+#   tags = {
+#     name = "demo-server2"
+#   }
+# }
