152b183458d59958109736f89a630d930b768e41
agp19d <agp19d@gmail.com>
2024-10-19 13:21:57 -0500

    modified structure

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "=>5.72.1"
-    }
+# EC2 instance to provision in us-east-1
+resource "aws_instance" "ec2-instance1" {
+  ami           = "ami-06b21ccaeff8cd686" # Amazon linux 3
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "demo-server1"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+# EC2 instance to provision in us-west-1
+resource "aws_instance" "ec2-instance2" {
+  provider      = aws.west
+  ami           = "ami-0cf4e1fcfd8494d5b" # Amazon linux 3
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "demo-server2"
+  }
 }
