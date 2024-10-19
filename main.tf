41285d54f695647d0c3349b6476ce08fb9e2fbc4
agp19d <agp19d@gmail.com>
2024-10-19 12:48:55 -0500

    changes to terraform scripts

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.72.1"
+      version = "=>5.72.1"
     }
   }
 }
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3bucket" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-alejandro"
 
   tags = {
     Name        = "s3-bucket"
