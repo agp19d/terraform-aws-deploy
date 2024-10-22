7d8133130a8e46734979fe61ec1d04344b653561
agp19d <agp19d@gmail.com>
2024-10-22 09:57:23 -0500

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,62 @@
-# EC2 instance to provision in us-east-1
-resource "aws_instance" "ec2-instance1" {
-  ami           = "ami-06b21ccaeff8cd686" # Amazon linux 3
-  instance_type = "t2.micro"
+# main.tf 
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    name = "demo-server1"
+    Name = "main-vpc"
+    Environment = "dev"
   }
 }
 
-# EC2 instance to provision in us-west-1
-resource "aws_instance" "ec2-instance2" {
-  provider      = aws.west
-  ami           = "ami-0cf4e1fcfd8494d5b" # Amazon linux 3
-  instance_type = "t2.micro"
+# Create a subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    name = "demo-server2"
+    Name = "main-subnet"
+    Environment = "dev"
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh"
+    Environment = "dev"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_spot_instance_request" "ec2-instance1" {
+  count = 3
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro"
+  spot_price      = "0.03"
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "demo-server1"
+    Environment = "dev"
   }
 }
